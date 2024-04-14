06bc97249acde895d08e7e7ad2c5283cc30fa37e
naokatu <naokatu4@gmail.com>
2024-04-14 14:24:05 +0900

    feat: APIを使うための準備

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,4 +2,6 @@ import createClient from 'openapi-fetch'
 
 import { type paths } from './schema'
 
-export const client = createClient<paths>({})
+export const client = createClient<paths>({
+  baseUrl: process.env['NEXT_PUBLIC_API_MOCK_URL'] ?? '',
+})
