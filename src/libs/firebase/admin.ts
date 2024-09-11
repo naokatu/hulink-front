a25628c602e70ec2e093fb94d81072274efa72c7
naokatu <naokatu4@gmail.com>
2024-09-11 12:25:59 +0900

    refactor: jsonファイルからではなく環境変数から取得

--- src/libs/firebase/admin.ts
+++ src/libs/firebase/admin.ts
@@ -2,7 +2,11 @@ import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require('../../../service-account.json')
+const serviceAccount = {
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  privateKey: process.env['FIREBASE_PRIVATE_KEY']?.replace(/\\n/g, '\n'),
+  clientEmail: process.env['FIREBASE_CLIENT_EMAIL'],
+}
 export const firebaseAdmin =
   getApps().length === 0
     ? initializeApp({
