059cbf907e643204c6728dbcf0aeacb45dfe6a52
naokatu <naokatu4@gmail.com>
2024-09-20 22:38:41 +0900

    feat: ログアウトボタンの追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,21 @@ import { type FC } from 'react'
 import { Flex, Box } from '@kuma-ui/core'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useSession } from 'next-auth/react'
+import { signOut as signOutByNextAuth, useSession } from 'next-auth/react'
+import { usePathname } from 'next/navigation'
 
 import HeaderImage from '../../../public/Header-icon_v1.png'
 import { SimpleButton } from '../SimpleButton'
 
 export const Header: FC = () => {
   const { data: session, status } = useSession()
+  const router = usePathname()
+
+  const handleSignOut = async (): Promise<void> => {
+    await signOutByNextAuth({
+      callbackUrl: '/',
+    })
+  }
 
   return (
     <header>
@@ -26,16 +34,24 @@ export const Header: FC = () => {
               alt="ヘッダーロゴ画像"
               className="h-16 w-16 mx-2"
               priority
-            ></Image>
+            />
           </Link>
         </Box>
         <Box className="ml-auto mx-3">
           {status === 'loading' ? (
             <SimpleButton bgColor={'bg-demoColor'}>読み込み中...</SimpleButton>
           ) : session !== null ? (
-            <Link href="/my-page">
-              <SimpleButton bgColor={'bg-baseColor'}>マイページへ</SimpleButton>
-            </Link>
+            router === '/my-page' ? (
+              <SimpleButton bgColor={'bg-unBaseColor'} onClick={handleSignOut}>
+                ログアウト
+              </SimpleButton>
+            ) : (
+              <Link href="/my-page">
+                <SimpleButton bgColor={'bg-baseColor'}>
+                  マイページへ
+                </SimpleButton>
+              </Link>
+            )
           ) : (
             <Link href="/login">
               <SimpleButton bgColor={'bg-baseColor'}>ログイン</SimpleButton>
